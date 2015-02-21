785b54e6682f8f6548e184a775ab7dd1369098ec
Nedjo Rogers <code@chocolatelilyweb.ca>
2015-02-21 11:30:26 -0800

    Issue #2429949: switch to using FileStorage to ensure re-provided config items are read from the correct extension.

--- src/ConfigSyncSnapshotter.php
+++ src/ConfigSyncSnapshotter.php
@@ -7,11 +7,10 @@
 
 namespace Drupal\config_sync;
 
-use Drupal\Core\Config\ExtensionInstallStorage;
+use Drupal\Core\Config\FileStorage;
+use Drupal\Core\Config\InstallStorage;
 use Drupal\Core\Config\StorageInterface;
 use Drupal\config_update\ConfigDiffInterface;
-use Drupal\config_update\ConfigListInterface;
-use Drupal\config_update\ConfigRevertInterface;
 
 /**
  * The ConfigSyncSnapshotter provides helper functions for taking snapshots of
@@ -27,25 +26,11 @@ class ConfigSyncSnapshotter implements ConfigSyncSnapshotterInterface {
   protected $configDiff;
 
   /**
-   * The config lister.
-   *
-   * @var \Drupal\config_update\ConfigListInterface
-   */
-  protected $configList;
-
-  /**
-   * The config reverter.
-   *
-   * @var \Drupal\config_update\ConfigRevertInterface
-   */
-  protected $configRevert;
-
-  /**
-   * The extension configuration storage.
+   * The active configuration storage.
    *
    * @var \Drupal\Core\Config\StorageInterface
    */
-  protected $extensionStorage;
+  protected $activeStorage;
 
   /**
    * The snapshot config storage.
@@ -59,20 +44,14 @@ class ConfigSyncSnapshotter implements ConfigSyncSnapshotterInterface {
    *
    * @param \Drupal\config_update\ConfigDiffInterface $config_diff
    *   The config differ.
-   * @param \Drupal\config_update\ConfigListInterface $config_list
-   *   The config lister.
-   * @param \Drupal\config_update\ConfigRevertInterface $config_revert
-   *   The config reverter.
    * @param \Drupal\Core\Config\StorageInterface $active_storage
    *   The active storage.
    * @param \Drupal\Core\Config\StorageInterface $snapshot_storage
    *   The snapshot storage.
    */
-  public function __construct(ConfigDiffInterface $config_diff, ConfigListInterface $config_list, ConfigRevertInterface $config_revert, StorageInterface $active_storage, StorageInterface $snapshot_storage) {
+  public function __construct(ConfigDiffInterface $config_diff, StorageInterface $active_storage, StorageInterface $snapshot_storage) {
     $this->configDiff = $config_diff;
-    $this->configList = $config_list;
-    $this->configRevert = $config_revert;
-    $this->extensionStorage = new ExtensionInstallStorage($active_storage);
+    $this->activeStorage = $active_storage;
     $this->snapshotStorage = $snapshot_storage;
   }
 
@@ -81,21 +60,44 @@ class ConfigSyncSnapshotter implements ConfigSyncSnapshotterInterface {
    */
   public function createExtensionSnapshot($type, $name) {
     // List the configuration items provided by the requested extension.
-    $item_names = array_keys($this->extensionStorage->getComponentNames($type, array($name)));
-    foreach ($item_names as $item_name) {
-      $extension_value = $this->configRevert->getFromExtension('', $item_name);
-      $active_value = $this->configRevert->getFromActive('', $item_name);
-      // If the active value is equivalent to the extension-provided one, use
-      // the active value so that it will include UUID values, used to
-      // determine rename changes..
-      if ($this->configDiff->same($extension_value, $active_value)) {
-        $value = $active_value;
+    if ($extension_storage = $this->getExtensionInstallStorage($type, $name)) {
+      $item_names = $extension_storage->listAll();
+      foreach ($item_names as $item_name) {
+        $extension_value = $extension_storage->read($item_name);
+        $active_value = $this->activeStorage->read($item_name);
+        // If the active value is equivalent to the extension-provided one, use
+        // the active value so that it will include UUID values, used to
+        // determine rename changes..
+        if ($this->configDiff->same($extension_value, $active_value)) {
+          $value = $active_value;
+        }
+        else {
+          $value = $extension_value;
+        }
+        $this->snapshotStorage->write($item_name, $value);
       }
-      else {
-        $value = $extension_value;
-      }
-      $this->snapshotStorage->write($item_name, $value);
     }
+  }
+
+  /**
+   * Returns a file storage object for a given extension's install directory,
+   * or FALSE if no such directory exists.
+   *
+   * @param string $type
+   *   The type of extension (module or theme).
+   * @param string $name
+   *   The machine name of the extension.
+   *
+   * @return
+   *   A FileStorage object for the given extension's install directory, or
+   *   FALSE if there is no such directory.
+   */
+  protected function getExtensionInstallStorage($type, $name) {
+    $config_path = drupal_get_path($type, $name) . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY;
+    if (is_dir($config_path)) {
+      return new FileStorage($config_path);
+    }
+    return FALSE;
   }
 
   /**
